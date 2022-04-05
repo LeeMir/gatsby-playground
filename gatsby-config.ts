40b629799189c618704c64ff05523ecea52ac0df
LeeMir <manofgodmj@gmail.com>
2022-04-05 11:47:15 +0900

    [Chore] init CMS

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,17 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://leemir-gatsby-playground.netlify.app/`,
+    title: `LeeMir Gatsby Playground`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    }, 
+  ],
 }
 
 export default config
