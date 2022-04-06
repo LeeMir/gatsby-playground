94db5c483a70a5347bc8cc5234c2db1962cf737e
이명재 <leemir@imyeongjaeui-MacBookPro.local>
2022-04-07 03:51:31 +0900

    [Chore] gatsby config: plugin들 연결
    최적화 플러그인
    fs, markdown용 플러그인

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,9 @@
-import type { GatsbyConfig } from 'gatsby'
+import type { GatsbyConfig } from 'gatsby';
+import path from 'path';
+
+require('dotenv').config({
+  path: `.env`,
+});
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -6,14 +11,20 @@ const config: GatsbyConfig = {
     title: `LeeMir Gatsby Playground`,
   },
   plugins: [
+    `gatsby-plugin-offline`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-remove-fingerprints`,
     {
-      resolve: `gatsby-plugin-netlify-cms`,
-    },
-    {
-      resolve: `gatsby-plugin-styled-components`
-    }
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `markdown-pages`,
+        path: path.resolve(`src/posts`),
+			},
+		},
+		`gatsby-transformer-remark`,
   ],
 };
-
 export default config;
