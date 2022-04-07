7dca4ad64c31e1d45fbefed90ab2a0ef021faa95
LeeMir <manofgodmj@gmail.com>
2022-04-07 17:40:26 +0900

    [Fix] query 수정
    slug는 frontmatter가 아니라 fields에서 가져와야한다라는 느낌 같음

--- src/templates/Post.tsx
+++ src/templates/Post.tsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+export const query = graphql`
+  query MyQuery($slug: String) {
+    markdownRemark(fields: {slug: {eq: $slug}}) {
+      frontmatter {
+        date
+        description
+        image
+        tags
+        title
+      }
+      html
+    }
+  }
+`;
+
 export default function Template({ data }: { data: any }) {
+  console.log(data);
   const { markdownRemark: { frontmatter, html } } = data;
   return (
     <div>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <img src={frontmatter.image} alt={frontmatter.title} />
+      <h1>{frontmatter.title}</h1>
+      <h2>{frontmatter.date}</h2>
+      <div
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </div>
   )
-}
-
-export const pageQuery = graphql`
-  query($slug: String) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        date(formatString: "YYYY년 MM월 DD일 ")
-        title
-        description
-        tags
-        image
-      }
-    }
-  }
-`
+};
